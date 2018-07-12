b8f3d4e15d7bbd61f3419406ba507341fcde2af9
Thomas Fanto <thomas@fanto.se>
2018-07-12 18:44:47 +0200

    .

---
+++ src/main/java/com/fnt/item/ItemForm.java
@@ -0,0 +1,12 @@
+package com.fnt.item;
+
+import com.fnt.entity.Item;
+import com.vaadin.ui.Window;
+
+public class ItemForm extends Window {
+
+	public ItemForm(ItemList itemList, ItemRepository customerRepository, String string, Item item, int crudCreate) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/main/java/com/fnt/item/ItemRepository.java
+++ src/main/java/com/fnt/item/ItemRepository.java
@@ -1,5 +1,22 @@
 package com.fnt.item;
 
+import java.util.List;
+
+import com.fnt.entity.Customer;
+import com.fnt.entity.Item;
+import com.fnt.sys.RestResponse;
+
 public class ItemRepository {
+
+	public RestResponse<List<Item>> search(String itemNumberStr, String descriptionStr, String orderingPointStr,
+			String inStockStr, String priceStr, String purchasePriceStr, String sortOrder) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public RestResponse<Item> getById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
