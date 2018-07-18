3eba35823c3d279fbdf9098740cc86f3f2851fb9
Thomas Fanto <thomas@fanto.se>
2018-07-18 14:15:04 +0200

    .

--- src/main/java/com/fnt/customerorder/CustomerOrderForm.java
+++ src/main/java/com/fnt/customerorder/CustomerOrderForm.java
@@ -3,18 +3,24 @@ package com.fnt.customerorder;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.fnt.dto.CustomerOrderLineListView;
 import com.fnt.entity.CustomerOrderHead;
 import com.fnt.entity.Item;
 import com.fnt.sys.Fnc;
 import com.fnt.sys.RestResponse;
 import com.vaadin.data.BeanValidationBinder;
+import com.vaadin.data.Binder;
 import com.vaadin.data.BindingValidationStatus;
 import com.vaadin.data.ValidationException;
+import com.vaadin.data.converter.StringToDoubleConverter;
+import com.vaadin.data.converter.StringToLongConverter;
+import com.vaadin.data.validator.BeanValidator;
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.DateField;
+import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextField;
@@ -26,6 +32,8 @@ public class CustomerOrderForm extends Window {
 
 	private Fnc fnc = new Fnc();
 
+	private Grid<CustomerOrderLineListView> grid = new Grid<>();
+
 	private static final long serialVersionUID = -214415727456373593L;
 	int crudFunction = 1;
 	CustomerOrderRepository customerOrderRepository = new CustomerOrderRepository();
@@ -36,15 +44,13 @@ public class CustomerOrderForm extends Window {
 	private ComboBox<String> customernumber = new ComboBox<>("Customernumber");
 	private ComboBox<String> name = new ComboBox<>("Name");
 	// lineInfo
-	
+
 	private ComboBox<String> itemnumber = new ComboBox<>("Itemnumber");
 	private ComboBox<String> itemndescription = new ComboBox<>("Description");
 	private TextField numberofitems = new TextField("Units");
 	private TextField priceperitem = new TextField("Price per Item");
-	private TextField linetotal = new TextField("Line total");
 	private Button btn_clearline = new Button("Clear");
 	private Button btn_addline = new Button("Add");
-	
 
 	private Button btn_cancel = new Button("Cancel");
 	private Button btn_save = new Button("Ok", VaadinIcons.CHECK);
@@ -71,44 +77,84 @@ public class CustomerOrderForm extends Window {
 		HorizontalLayout buttons = new HorizontalLayout(btn_cancel, btn_save);
 		buttons.setSpacing(true);
 
+		// HEADER - registration / selection
+
 		HorizontalLayout orderheader = new HorizontalLayout();
+		orderheader.setSpacing(false);
 		orderheader.addComponent(orderdate);
 		orderheader.addComponent(customernumber);
 		orderheader.addComponent(name);
 
 		HorizontalLayout orderline = new HorizontalLayout();
-
+		orderline.setSpacing(false);
 		orderline.addComponent(itemnumber);
 		orderline.addComponent(itemndescription);
 		orderline.addComponent(numberofitems);
 		orderline.addComponent(priceperitem);
-		orderline.addComponent(linetotal);
-		
-		HorizontalLayout orderlineButtons = new HorizontalLayout();
 
+		HorizontalLayout orderlineButtons = new HorizontalLayout();
+		orderlineButtons.setSpacing(false);
 		orderlineButtons.addComponent(btn_clearline);
 		orderlineButtons.addComponent(btn_addline);
-		//orderlineButtons.setComponentAlignment(btn_clearline, Alignment.BOTTOM_LEFT);
-		//orderlineButtons.setComponentAlignment(btn_addline, Alignment.BOTTOM_RIGHT);
 		orderline.addComponent(orderlineButtons);
 		orderline.setComponentAlignment(orderlineButtons, Alignment.BOTTOM_LEFT);
-		
-
-		
-
-		
 
 		VerticalLayout orderform = new VerticalLayout();
 		orderform.addComponent(orderheader);
 		orderform.addComponent(orderline);
 
-		VerticalLayout layout = new VerticalLayout(orderform, buttons);
-		layout.setComponentAlignment(orderform, Alignment.TOP_LEFT);
-		layout.setComponentAlignment(buttons, Alignment.BOTTOM_RIGHT);
+		// @formatter:off
+		// grid show whats in the order 
+	       Binder<CustomerOrderLineListView> binder = grid.getEditor().getBinder();
+
+	        grid.addColumn(CustomerOrderLineListView::getItemnumber)
+	        .setCaption("Itemnumber")
+	        .setEditorBinding(binder
+	                .forField(new TextField())
+	                .withNullRepresentation("")
+	                .withValidator(new BeanValidator(CustomerOrderLineListView.class, "itemnumber"))
+	                .bind(CustomerOrderLineListView::getItemnumber, CustomerOrderLineListView::setItemnumber));
+	        grid.addColumn(CustomerOrderLineListView::getDescription)
+	        .setCaption("Description")
+	        .setEditorBinding(binder
+	                .forField(new TextField())
+	                .withNullRepresentation("")
+	                .withValidator(new BeanValidator(CustomerOrderLineListView.class, "description"))
+	                .bind(CustomerOrderLineListView::getDescription, CustomerOrderLineListView::setDescription));
+	        grid.addColumn(CustomerOrderLineListView::getUnits)
+	        .setCaption("Units")
+	        .setEditorBinding(binder
+	                .forField(new TextField())
+	                .withConverter(new StringToLongConverter("Please enter a number"))
+	                .withValidator(new BeanValidator(CustomerOrderLineListView.class, "units"))
+	                .bind(CustomerOrderLineListView::getUnits, CustomerOrderLineListView::setUnits));
+	        grid.addColumn(CustomerOrderLineListView::getPriceperitem)
+	        .setCaption("Price per item")
+	        .setEditorBinding(binder
+	                .forField(new TextField())
+	                .withConverter(new StringToDoubleConverter("Please enter a number"))
+	                .withValidator(new BeanValidator(CustomerOrderLineListView.class, "priceoeritem"))
+	                .bind(CustomerOrderLineListView::getPriceperitem, CustomerOrderLineListView::setPriceperitem));
+	        grid.addColumn(CustomerOrderLineListView::getLinetotal)
+	        .setCaption("Line total")
+	        .setEditorBinding(binder
+	                .forField(new TextField())
+	                .withConverter(new StringToDoubleConverter("Please enter a number"))
+	                .withValidator(new BeanValidator(CustomerOrderLineListView.class, "linetotal"))
+	                .bind(CustomerOrderLineListView::getLinetotal, CustomerOrderLineListView::setLinetotal));
+		
+		
+	        grid.setSizeFull();
+		
+			// @formatter:on
+
+		// VerticalLayout layout = new VerticalLayout(orderform, grid, buttons);
+		VerticalLayout layout = new VerticalLayout();
+		layout.addComponents(orderform, grid,buttons);
 		setContent(layout);
 		setModal(true);
 		setSizeFull();
-		center();
+		//center();
 	}
 
 	private void initBehavior(CustomerOrderHead orderHead) {
