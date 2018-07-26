49f087c2060185bbd4d5223def94f89b50f36009
Thomas Fanto <thomas@fanto.se>
2018-07-26 23:37:43 +0200

    contemporary menu

---
+++ src/main/java/com/fnt/ui/DefaultView.java
@@ -0,0 +1,13 @@
+package com.fnt.ui;
+
+import com.vaadin.navigator.View;
+import com.vaadin.ui.Composite;
+import com.vaadin.ui.Label;
+
+public class DefaultView extends Composite implements View {
+
+	public DefaultView() {
+		setCompositionRoot(new Label("Default"));
+	}
+
+}
